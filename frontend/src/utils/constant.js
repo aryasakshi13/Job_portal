092cda1f4ec2fe65a828cc4fd1bbd2f3eb9a9d05
aryasakshi13 <sakshiarya884@gmail.com>
2026-04-18 19:02:07 +0530

    updating fronend url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,14 @@
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:3000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:3000/api/v1/company";
+// export const USER_API_END_POINT="http://localhost:3000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:3000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:3000/api/v1/application";
+// export const COMPANY_API_END_POINT="http://localhost:3000/api/v1/company";
+
+
+// Remove the '/' from the end of the base URL
+const BASE_URL = "https://job-portal-r8k4.onrender.com";
+
+// Build the endpoints carefully
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
