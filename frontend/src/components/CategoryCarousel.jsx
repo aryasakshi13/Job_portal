c1d2afdef79c1786274237c42b6a8bff0adcb9a5
aryasakshi13 <sakshiarya884@gmail.com>
2026-04-18 21:01:24 +0530

    Fix the slidebar issue

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -22,19 +22,24 @@ const CategoryCarousel = () => {
     }
 
     return (
-        <div>
-            <Carousel className="w-full max-w-xl mx-auto my-20">
-                <CarouselContent>
+        <div className='w-full px-12 md:px-10'>
+            <Carousel 
+            opts={{
+                    align: "start",
+                    loop: true,
+                }}
+            className="w-full max-w-xl mx-auto my-20">
+                <CarouselContent className="-ml-1">
                     {
                         category.map((cat, index) => (
-                            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
-                                <Button onClick={()=>searchJobHandler(cat)} variant="outline" className="rounded-full">{cat}</Button>
+                            <CarouselItem  key={index} className="pl-1 basis-full md:basis-1/2 lg-basis-1/3 flex justify-center">
+                                <Button onClick={()=>searchJobHandler(cat)} variant="outline" className="rounded-full whitespace-nowrap text-sm md:text-base px-4 py-2">{cat}</Button>
                             </CarouselItem>
                         ))
                     }
                 </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
+                <CarouselPrevious className="flex items-center justify-center" />
+                <CarouselNext className="flex items-center justify-center" />
             </Carousel>
         </div>
     )
